ac7896b40879cb63d345c44a313244b7540768cc
Duda Mafra <mariaszmafra@gmail.com>
2022-10-16 00:16:18 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+export function  App() {
+  return (
+    <>
+      <h1 className="text-gray-900 text-2xl">Hello</h1>
+      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
+        Enviar
+      </button>
+    </>
+  )
+}
