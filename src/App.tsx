fc568fa47a8ff3e4149e1d66abb9b4a19fe20ee6
Duda Mafra <mariaszmafra@gmail.com>
2022-10-16 21:33:36 -0300

    After

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
+import { SignIn } from './pages/SignIn';
 import './styles/global.css';
 
 export function  App() {
   return (
-    <>
-      <h1 className="text-gray-900 text-2xl">Hello</h1>
-      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
-        Enviar
-      </button>
-    </>
+    <SignIn/>
   )
 }
